9234db7772100c05a139b43de9b43d9b0ec30c19
yoyo1025 <hsjejxys@gmail.com>
2025-02-02 12:32:22 +0900

    ボタンを中央寄せ

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <main className="flex justify-center">
       <Button>Hello Shadcn</Button>
     </main>
   );
