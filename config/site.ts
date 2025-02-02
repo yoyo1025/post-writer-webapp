7a0590f4f20532d713ba9c87e1fdb71b6f14c413
yoyo1025 <hsjejxys@gmail.com>
2025-02-02 23:45:10 +0900

    metadata作成

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ import { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
   name: "Post Writer",
-  description: "",
+  description: "ブログ投稿ができるWebアプリケーションです。",
   url: "http:localhost:3000",
   ogImage: "",
   links: {
